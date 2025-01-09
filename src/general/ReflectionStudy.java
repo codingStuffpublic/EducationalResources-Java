3fc2ec9a90782c8d21be276791ea6abbaa4f6183
Sophie <zsofiliszkai@gmail.com>
2025-01-09 15:56:38 +0100

    Singleton, Reflection

---
+++ src/general/ReflectionStudy.java
@@ -0,0 +1,28 @@
+package general;
+
+import java.lang.reflect.Constructor;
+
+import designpatterns.LazySingleton;
+
+public class ReflectionStudy {
+
+	public static void main(String[] args) {
+		LazySingleton instanceOne = LazySingleton.getInstance();
+		LazySingleton instanceTwo = null;
+		try {
+			Constructor[] constructors = LazySingleton.class.getDeclaredConstructors();
+			for (Constructor constructor : constructors) {
+				// java.lang.IllegalAccessException: class ReflectionStudy cannot access a
+				// member of
+				// class LazyInitializedSingleton with modifiers "private"
+				constructor.setAccessible(true);
+				instanceTwo = (LazySingleton) constructor.newInstance();
+				break;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		System.out.println(instanceOne.hashCode());
+		System.out.println(instanceTwo.hashCode());
+	}
+}
